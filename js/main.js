aec38ffb3fbb246e765dd6071afdda7b7d8bc1ef
Aman Kumar <isharaman8@gmail.com>
2021-11-17 19:38:57 +0530

    updated files: created player class, added run stop and update methods to it

--- js/main.js
+++ js/main.js
@@ -15,23 +15,102 @@ renderer.setClearColor(0xb7c3f3, 1);
 const light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
 
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+//GLOBAL VARIABLE
+const start_position = 3;
+const end_position = -start_position;
+
+function createCube(size, positionX, rotY = 0, color = 0xfbc851) {
+	const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+	const material = new THREE.MeshBasicMaterial({ color: color });
+	const cube = new THREE.Mesh(geometry, material);
+	cube.position.x = positionX;
+	cube.rotation.y = rotY;
+	scene.add(cube);
+	return cube;
+}
 
 camera.position.z = 5;
 
 var loader = new THREE.GLTFLoader();
-loader.load("../models/scene.gltf", (gltf) => {
-	scene.add(gltf.scene);
-	gltf.scene.scale.set(0.4, 0.4, 0.4);
-	gltf.scene.position.set(0, -1, 0);
-});
+
+function delay(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+class Doll {
+	constructor() {
+		loader.load("../models/scene.gltf", (gltf) => {
+			scene.add(gltf.scene);
+			gltf.scene.scale.set(0.4, 0.4, 0.4);
+			gltf.scene.position.set(0, -1, 0);
+			this.doll = gltf.scene;
+		});
+	}
+	lookBackward() {
+		// this.doll.rotation.y = 3.15;
+		gsap.to(this.doll.rotation, { y: -3.15, duration: 0.45 });
+	}
+	lookForward() {
+		// this.doll.rotation.y = 0;
+		gsap.to(this.doll.rotation, { y: 0, duration: 0.45 });
+	}
+
+	start() {
+		this.lookBackward();
+	}
+}
+// setTimeout(() => {
+// 	doll.lookBackward();
+// }, 1000);
+//CREATE TRACK FUNCTION
+function createTrack() {
+	createCube(
+		{ w: start_position * 2 + 0.2, h: 1.5, d: 1 },
+		0,
+		0,
+		0xe5a716
+	).position.z = -1;
+	createCube({ w: 0.2, h: 1.5, d: 1 }, start_position, -0.35);
+	createCube({ w: 0.2, h: 1.5, d: 1 }, end_position, 0.35);
+}
+
+createTrack();
+
+class Player {
+	constructor() {
+		const geometry = new THREE.SphereGeometry(0.3, 32, 16); //radius, height, width
+		const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+		const sphere = new THREE.Mesh(geometry, material);
+		sphere.position.x = start_position;
+		sphere.position.z = 1;
+		scene.add(sphere);
+		this.player = sphere;
+		this.playerInfo = {
+			positionX: start_position,
+			velocity: 0,
+		};
+	}
+	run() {
+		this.playerInfo.velocity = 0.03;
+	}
+
+	stop() {
+		// this.playerInfo.velocity = 0;
+		gsap.to(this.playerInfo, { velocity: 0, duration: 0.2 });
+	}
+
+	update() {
+		this.player.position.x -= this.playerInfo.velocity;
+	}
+}
+const player = new Player();
+
+let doll = new Doll();
 
 function animate() {
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
+	player.update();
 }
 animate();
 window.addEventListener("resize", onWindowResize, false);
@@ -42,3 +121,15 @@ function onWindowResize() {
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
+window.addEventListener("keydown", (e) => {
+	if (e.key == "ArrowUp") {
+		player.run();
+	}
+});
+
+window.addEventListener("keyup", (e) => {
+	if (e.key == "ArrowUp") {
+		player.stop();
+	}
+});
